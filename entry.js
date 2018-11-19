d5adfabaf07aaaa0845df6d3f9b62c063163ba3a
“baxi” <wanbao_chen@qingsongchou.com>
2018-11-19 15:19:29 +0800

    add

--- entry.js
+++ entry.js
@@ -1,1 +1,7 @@
-document.getElementById('app').innerHTML ='这是我的第一个打包成功的程序哈'
+document.getElementById('app').innerHTML ='这是我的第一个打包成功的程序哈';
+
+// 添加 另一个js文件
+require("./first.js");
+
+// 添加style
+require("!style-loader!css-loader!./style.css");
